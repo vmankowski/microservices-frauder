01c26cf97d0ab180eec3a6c252c67a0ca90a06d7
Volodymyr Mankivskyi <volodymyr.mankivskyi@fourth.com>
2025-03-22 13:15:20 +0100

    module 4: added fraud service; implement store function; added postgresql config; added interconnection between CUSTOMER and FRAUD

---
+++ fraud/src/test/java/org/msa/one/fraud/FraudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.msa.one.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
